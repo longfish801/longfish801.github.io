ddeb7153e4886f6cc04add6ff4c321e09038a7e5
io.github.longfish801 <22591607+longfish801@users.noreply.github.com>
2022-11-27 09:13:08 +0900

    『おまえも犯人だ』登録

--- docs/_cgi/infobar/infobar.js
+++ docs/_cgi/infobar/infobar.js
@@ -24,7 +24,7 @@ infobar({
         {
             "width": "88",
             "alt": "デデフラ",
-            "src": "/_style/img/ban_ddfl.gif",
+            "src": "/ddfl/img/banner.gif",
             "href": "/ddfl/index.html",
             "height": "31"
         }
