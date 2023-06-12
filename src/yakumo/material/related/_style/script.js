72802d17bf221896ecfd431d280aa47a7cef6ce2
io.github.longfish801 <22591607+longfish801@users.noreply.github.com>
2023-06-12 19:48:42 +0900

    relatedのコピー処理を削除

--- src/yakumo/material/related/_style/script.js
+++
@@ -1,8 +0,0 @@
-
-function abreact(){
-	var objList = document.getElementsByClassName('hidden');
-	objList.each(function(obj){
-		obj.style.color = '#333333';
-		obj.style.background = '#CCCCCC';
-	});
-}
